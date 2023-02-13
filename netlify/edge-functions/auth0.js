f4006bc564378d53684f1748f35e6599fd39912f
Scio <sayantan.chaudhuri@gmail.com>
2023-02-13 07:54:12 +0000

    More data

--- netlify/edge-functions/auth0.js
+++ netlify/edge-functions/auth0.js
@@ -6,25 +6,29 @@ const CLIENTSECRET = Deno.env.get('CLIENTSECRET')
 
 async function getToken() {
     const domain = `${TENANT}.us.auth0.com`
-    // Default options are marked with *
-    const response = await fetch(
-        `https://${domain}/oauth/token`, 
-        {
-            method: 'POST',
-            cache: 'no-cache',
-            headers: {
-            // 'Content-Type': 'application/json'
-            'Content-Type': 'application/x-www-form-urlencoded',
-            },
-            body: JSON.stringify(new URLSearchParams({
-                grant_type: 'client_credentials',
-                client_id: `${CLIENTID}`,
-                client_secret: `${CLIENTSECRET}`,
-                audience: `https://${domain}/api/v2/`
-            })),
-        }
-    );
-    return response; // parses JSON response into native JavaScript objects
+    try {
+        // Default options are marked with *
+        const response = await fetch(
+            `https://${domain}/oauth/token`, 
+            {
+                method: 'POST',
+                cache: 'no-cache',
+                headers: {
+                // 'Content-Type': 'application/json'
+                'Content-Type': 'application/x-www-form-urlencoded',
+                },
+                body: JSON.stringify(new URLSearchParams({
+                    grant_type: 'client_credentials',
+                    client_id: `${CLIENTID}`,
+                    client_secret: `${CLIENTSECRET}`,
+                    audience: `https://${domain}/api/v2/`
+                })),
+            }
+        );
+        return response; // parses JSON response into native JavaScript objects
+    } catch (e) {
+        return e;
+    }
 }
 
-export default async (request, context) => new Response(await JSON.stringify(getToken()))
+export default async (request, context) => new Response(await JSON.stringify({request, response: getToken()}, null, 2))
