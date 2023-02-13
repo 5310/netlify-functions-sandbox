33b3bd6a06c1315079e8af763c8a8ea9d342be26
Scio <sayantan.chaudhuri@gmail.com>
2023-02-13 10:32:18 +0000

    More

--- netlify/edge-functions/auth0.js
+++ netlify/edge-functions/auth0.js
@@ -1,43 +1,40 @@
 export const config = { path: "/auth0" }
 
-const TENANT = Deno.env.get('TENANT')
-const CLIENTID = Deno.env.get('CLIENTID')
-const CLIENTSECRET = Deno.env.get('CLIENTSECRET')
+const DOMAIN = Deno.env.get('AUTH0_DOMAIN')
+const CLIENTID = Deno.env.get('AUTH0_CLIENTID')
+const CLIENTSECRET = Deno.env.get('AUTH0_CLIENTSECRET')
 
 async function getToken() {
-    const domain = `${TENANT}.us.auth0.com`
     try {
-        // Default options are marked with *
         const response = await fetch(
-            `https://${domain}/oauth/token`, 
+            `https://${DOMAIN}/oauth/token`, 
             {
                 method: 'POST',
                 cache: 'no-cache',
                 cors: 'no-cors',
                 headers: {
-                // 'Content-Type': 'application/json'
-                'Content-Type': 'application/x-www-form-urlencoded',
+                    'Content-Type': 'application/x-www-form-urlencoded',
                 },
                 body: JSON.stringify(new URLSearchParams({
                     grant_type: 'client_credentials',
                     client_id: `${CLIENTID}`,
                     client_secret: `${CLIENTSECRET}`,
-                    audience: `https://${domain}/api/v2/`
+                    audience: `https://${DOMAIN}/api/v2/`
                 })),
             }
         );
-        return response; // parses JSON response into native JavaScript objects
+        return response.json();
     } catch (e) {
         return e;
     }
 }
 
-export default async (request, context) => new Response(await JSON.stringify({request, response: getToken()}, null, 2))
-
-curl --request POST \
-  --url 'https://dev-n84xs2sw8tzfloyk.us.auth0.com/oauth/token' \
-  --header 'content-type: application/x-www-form-urlencoded' \
-  --data grant_type=client_credentials \
-  --data 'client_id=261aDeOPIiMTi1pYcuT8KcmlZF3usU9d' \
-  --data 'client_secret=sjMwc6g1fqiSegP1_66gYtuLIJ3VYaZIwFdeJ9pwYeYsekhapkfUrLM8_L0ioAxh' \
-  --data 'audience=https://dev-n84xs2sw8tzfloyk.us.auth0.com/api/v2/'
+export default async (request, context) => 
+    new Response(JSON.stringify(
+        {
+            request, 
+            response: await getToken()
+        }, 
+        null, 
+        2
+    ))
