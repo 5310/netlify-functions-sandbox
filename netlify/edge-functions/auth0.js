bd7b4381ba031cc16c09785069de483788c9c385
Scio <sayantan.chaudhuri@gmail.com>
2023-02-14 05:43:13 +0000

    Redirect

--- netlify/edge-functions/auth0.js
+++ netlify/edge-functions/auth0.js
@@ -5,68 +5,68 @@ const CLIENTID = Deno.env.get('AUTH0_CLIENTID')
 const CLIENTSECRET = Deno.env.get('AUTH0_CLIENTSECRET')
 
 async function getToken() {
-    try {
-        const response = await fetch(
-            `https://${DOMAIN}/oauth/token`, 
-            {
-                method: 'POST',
-                cache: 'no-cache',
-                cors: 'no-cors',
-                headers: {
-                    'Content-Type': 'application/x-www-form-urlencoded',
-                },
-                body: (new URLSearchParams({
-                    grant_type: 'client_credentials',
-                    client_id: `${CLIENTID}`,
-                    client_secret: `${CLIENTSECRET}`,
-                    audience: `https://${DOMAIN}/api/v2/`
-                })).toString(),
-            }
-        );
-        return await response.json();
-    } catch (e) {
-        return e;
-    }
+  try {
+    const response = await fetch(
+      `https://${DOMAIN}/oauth/token`,
+      {
+        method: 'POST',
+        cache: 'no-cache',
+        cors: 'no-cors',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: (new URLSearchParams({
+          grant_type: 'client_credentials',
+          client_id: `${CLIENTID}`,
+          client_secret: `${CLIENTSECRET}`,
+          audience: `https://${DOMAIN}/api/v2/`
+        })).toString(),
+      }
+    );
+    return await response.json();
+  } catch (e) {
+    return e;
+  }
 }
 
 async function getUser(token, user_id) {
-    try {
-        const response = await fetch(
-            `https://${DOMAIN}/api/v2/users/${encodeURI(user_id)}`, 
-            {
-                cache: 'no-cache',
-                cors: 'no-cors',
-                headers: {
-                    'authorization': `Bearer ${token}`,
-                    'content-type': 'application/json'
-                },
-            }
-        );
-        return response.json();
-    } catch (e) {
-        console.log('this is in error')
-        return e;
-    }
+  try {
+    const response = await fetch(
+      `https://${DOMAIN}/api/v2/users/${encodeURI(user_id)}`,
+      {
+        cache: 'no-cache',
+        cors: 'no-cors',
+        headers: {
+          'authorization': `Bearer ${token}`,
+          'content-type': 'application/json'
+        },
+      }
+    );
+    return response.json();
+  } catch (e) {
+    console.log('this is in error')
+    return e;
+  }
 }
 
 export default async (request, context) => {
-    const ip = context.ip
-    const user_id = context.cookies.get('user_id')
-    const token = (await getToken()).access_token
-    const user = await getUser(token, user_id)
-    console.log({
-        ip, 
-        user_id, 
-        token, 
-        user
-    })
-    return new Response(JSON.stringify(
-        {
-            ip,
-            user_id,
-            user,
-        }, 
-        null, 
-        2
-    ))
+  const ip = context.ip
+  const user_id = context.cookies.get('user_id')
+  const token = (await getToken()).access_token
+  const user = await getUser(token, user_id)
+  console.log({
+    ip,
+    user_id,
+    token,
+    user
+  })
+  return new Response(JSON.stringify(
+    {
+      ip,
+      user_id,
+      user,
+    },
+    null,
+    2
+  ))
 }
--- netlify/functions/auth0.js
+++ netlify/functions/auth0.js
@@ -1,6 +1,8 @@
 exports.handler = async function (event, context) {
-    return {
-        statusCode: 200,
-        body: JSON.stringify({ message: "Hello World" }),
-    };
-};
+  return {
+    statusCode: 302,
+    headers: {
+      'Location': 'https://dev-n84xs2sw8tzfloyk.us.auth0.com/authorize?response_type=id_token&response_mode=form_post&client_id=261aDeOPIiMTi1pYcuT8KcmlZF3usU9d&redirect_uri=https://sage-conkies-0fe563.netlify.app&nonce=123456789',
+    },
+  }
+}
