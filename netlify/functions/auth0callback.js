aa52b8cf6a68859917a94d21b04e3d0ee094bbe4
Scio <sayantan.chaudhuri@gmail.com>
2023-02-14 11:27:29 +0000

    more

--- netlify/functions/auth0callback.js
+++ netlify/functions/auth0callback.js
@@ -1,16 +1,19 @@
 const DOMAIN = process.env.AUTH0_DOMAIN
 const CLIENTID = process.env.AUTH0_CLIENTID
-const URL = process.env.URL
+const SITEURL = process.env.URL
 
 exports.handler = async function (event, context) {
   const id_token = (new URLSearchParams(event.body)).get('id_token') ?? ''
-
+  const url = new URL(SITEURL)
+  url.search = new URLSearchParams({
+    id_token,
+  })
   return {
-    statusCode: 302,
-    headers: {
-      'Set-Cookie': `id_token=${id_token}; Secure`, // won't be visible on the frontend anyway, won't show up at all
-      'Location': URL,
-    },
+    // statusCode: 302,
+    // headers: {
+    //   'Set-Cookie': `id_token=${id_token}; Secure`, // won't be visible on the frontend anyway, won't show up at all
+    //   'Location': SITEURL,
+    // },
     // statusCode: 200,
     // headers: {
     //   'Set-Cookie': `id_token=${id_token}; Secure`, // won't be visible to frontend anyway, still won't show up
@@ -18,9 +21,13 @@ exports.handler = async function (event, context) {
     // body: `
     //   <!DOCTYPE html>
     //   <html>
-    //   <head><meta http-equiv="refresh" content="0; url='${URL}'"></head>
+    //   <head><meta http-equiv="refresh" content="0; url='${SITEURL}'"></head>
     //   <body>${id_token}</body>
     //   </html>
     // `
+    statusCode: 302,
+    headers: {
+      'Location': url.href,
+    },
   }
 }
