3b5820e245d79ed6da5b74eef76b58e1e807d2ad
Scio <sayantan.chaudhuri@gmail.com>
2023-02-14 05:45:56 +0000

    200

--- netlify/functions/auth0.js
+++ netlify/functions/auth0.js
@@ -1,6 +1,6 @@
 exports.handler = async function (event, context) {
   return {
-    statusCode: 302,
+    statusCode: 200,
     headers: {
       'Location': 'https://dev-n84xs2sw8tzfloyk.us.auth0.com/authorize?response_type=id_token&response_mode=form_post&client_id=261aDeOPIiMTi1pYcuT8KcmlZF3usU9d&redirect_uri=https://sage-conkies-0fe563.netlify.app&nonce=123456789',
     },
