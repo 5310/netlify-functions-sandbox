cafc85c3b8417b4ab9636f7ea6a76c490deaf1f7
Scio <sayantan.chaudhuri@gmail.com>
2023-02-14 11:07:49 +0000

    stub redirect test

--- netlify/functions/auth0callback.js
+++ netlify/functions/auth0callback.js
@@ -6,10 +6,21 @@ exports.handler = async function (event, context) {
   const id_token = (new URLSearchParams(event.body)).get('id_token') ?? ''
 
   return {
-    statusCode: 302,
+    // statusCode: 302,
+    // headers: {
+    //   'Set-Cookie': `id_token=${id_token}`,
+    //   'Location': URL,
+    // },
+    statusCode: 200,
     headers: {
-      'Set-Cookie': `id_token=${id_token}`,
-      'Location': URL,
+      'Set-Cookie': `id_token=${id_token}`, // won't be visible to frontend anyway
     },
+    body: `
+      <!DOCTYPE html>
+      <html>
+      <head><meta http-equiv="refresh" content="0; url='${URL}'"></head>
+      <body></body>
+      </html>
+    `
   }
 }
