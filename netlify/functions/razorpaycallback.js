56aadaa372a532d80ae39344755390b54edcae28
Scio <sayantan.chaudhuri@gmail.com>
2023-02-17 11:37:23 +0000

    callback

---
+++ netlify/functions/razorpaycallback.js
@@ -0,0 +1,22 @@
+const cookie = require('cookie')
+const Razorpay = require('razorpay')
+
+const SITEURL = process.env.URL
+
+const AUTH0_DOMAIN = process.env.AUTH0_DOMAIN
+const AUTH0_CLIENTID = process.env.AUTH0_CLIENTID
+const AUTH0_WEB_CLIENTID = process.env.AUTH0_WEB_CLIENTID
+
+const RAZORPAY_ID = process.env.RAZORPAY_ID
+const RAZORUPAY_SECRET = process.env.RAZORPAY_SECRET
+const razorpay = new Razorpay({ key_id: RAZORPAY_ID, key_secret: RAZORUPAY_SECRET })
+
+
+exports.handler = async function (event, context) {
+    const params = new URLSearchParams(event.body)
+    console.log(params)
+    return {
+        statusCode: 200,
+        body: JSON.stringify(params, null, 2),
+    }
+}
